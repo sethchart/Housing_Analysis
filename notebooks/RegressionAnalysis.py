dd024d8d6eee46d25fd6f2ffbb70bfc884532ca6
Seth Chart <seth.chart@protonmail.com>
2020-10-08 22:41:45 -0400

    Working on price model.

--- notebooks/RegressionAnalysis.py
+++ notebooks/RegressionAnalysis.py
@@ -12,6 +12,7 @@ from scipy import stats
 from statsmodels import api as sm
 from statsmodels.stats.outliers_influence import variance_inflation_factor as vif
 
+
 class RegressionAnalysis(object):
 
 
@@ -23,7 +24,6 @@ class RegressionAnalysis(object):
         """Load the cleaned data as a data frame."""
         df = pd.read_csv(self.cleaned_data_path)
         return df
-
 
 
     def make_pair_plot(self, df):
@@ -44,6 +44,79 @@ class RegressionAnalysis(object):
         return model
 
 
+    def predict_target(self, X, model):
+        """Use the provided model and data X to produce predicted values of the target."""
+        y_hat = model.predict(X)
+        return y_hat
+
+
+    def compute_residuals(self, y, y_hat):
+        """Compute residuals from a series of observed values y and predicted values y_hat"""
+        return y-y_hat
+
+
+    def plot_residuals_against_inputs(self, X, residuals):
+        """Plot residuals against input variables."""
+        number_of_plots = len(X.columns)
+        nrows = int(np.sqrt(number_of_plots))
+        ncols = int(number_of_plots/nrows)
+        fig, axs = plt.subplots(nrows=nrows, ncols=ncols)
+        fig.set_figheight(6*nrows)
+        fig.set_figwidth(8*ncols)
+        for ax, col in zip(axs.flat, X.columns):
+            sns.scatterplot(x=X[col], y=residuals, ax=ax)
+            sns.lineplot(x=[X[col].min(), X[col].max()], y=[0, 0], ax=ax, color='red')
+            ax.set_xlabel(col.title())
+            ax.set_ylabel('Residuals')
+            ax.set_title(f'Residuals vs {col.title()}')
+
+
+    def plot_residuals_against_prediction(self, y_hat, residuals):
+        """Plot residuals against predicted values."""
+        ax = sns.scatterplot(
+            x=y_hat, 
+            y=residuals
+        )
+        sns.lineplot(x=[y_hat.min(), y_hat.max()], y=[0, 0], color='red', ax=ax)
+        ax.set(
+            title='Residuals vs Predicted Values',
+            xlabel='Predicted Values',
+            ylabel='Residuals'
+        )
+
+
+
+    def plot_residuals_distribution(self, residuals):
+        """Plot the distribution of normalized residuals."""
+        normalized_residuals = residuals/residuals.std()
+        ax = sns.distplot(normalized_residuals)
+        ax.set(
+            title='Distribution of Normalized Residuals.',
+            xlabel='Residuals',
+            ylabel='Probability Density'
+        )
+
+
+    def plot_residuals_normal_qq(self, residuals):
+        """Make a QQ ploat of normalized residuals against a standard normal distribution."""
+        plt.style.use('ggplot')
+        normalized_residuals = residuals/residuals.std()
+        sm.graphics.qqplot(normalized_residuals, dist=stats.norm, line='45', fit=True)
+        kstest_result = stats.kstest(normalized_residuals, 'norm')
+        if kstest_result.pvalue <= 0.05:
+            print(f'We reject the null hypothesis that our residuals are normally distributed at the alpha =0.05 level.')
+        else:
+            print(f'We fail to reject the null hypothesis that our residuals are normally distributed at the alpha = 0.05 level.')
+
+
+    def compute_vif(self, X):
+        """Compute variance inflation factors for all input variables."""
+        vifs = dict()
+        for ind, col in enumerate(X.columns):
+            vif_score = vif(np.matrix(X), ind)
+            vifs[col] = vif_score.round(2)
+        return vifs
+
 
 class _TestRegressionAnalysis(RegressionAnalysis):
 
@@ -52,13 +125,12 @@ class _TestRegressionAnalysis(RegressionAnalysis):
         super().__init__()
         np.random.seed(42)
         self.df = pd.DataFrame(np.random.randint(0,100,size=(10, 4)), columns=list('ABCD'))
-        self.X = df.drop('A', axis=1)
-        self.y = df['A']
+        self.X = self.df.drop('A', axis=1)
+        self.y = self.df['A']
         self.linreg = LinearRegression(fit_intercept=True)
-        self.model = linreg.fit(X, y)
-        self.y_hat = model.predict(X)
-        self.residuals = y-y_hat
-
+        self.model = self.linreg.fit(self.X, self.y)
+        self.y_hat = self.model.predict(self.X)
+        self.residuals = self.y-self.y_hat
 
 
     def test_load_data(self):
@@ -91,7 +163,6 @@ class _TestRegressionAnalysis(RegressionAnalysis):
         return test_passes
 
 
-
     def test_fit_model(self):
         """Test fit_model function."""
         try:
@@ -104,199 +175,76 @@ class _TestRegressionAnalysis(RegressionAnalysis):
             test_passes = False
         return test_passes
 
-#
-#test_fit_model()
-#
-#
-## ## Function to predict target from input variabes
-#
-## In[ ]:
-#
-#
-#def predict_target(X, model):
-#    """Use the provided model and data X to produce predicted values of the target."""
-#    y_hat = model.predict(X)
-#    return y_hat
-#
-#def test_predict_target():
-#    """Test predict_target function."""
-#    try:
-#        y_hat = predict_target(X, model) 
-#        prediction_match = y_hat.round(2) == np.array(
-#            [29.28, 69.42, 78.69, 22.02, 13.6, 15.76, 15.28, 53.9, 53.48, 33.56]
-#        )
-#        test_passes = prediction_match.min()
-#    except:
-#        test_passes = False
-#    return test_passes
-#
-#test_predict_target()
-#
-#
-## ## Function to compute residuals
-#
-## In[ ]:
-#
-#
-#def compute_residuals(y, y_hat):
-#    """Compute residuals from a series of observed values y and predicted values y_hat"""
-#    return y-y_hat
-#
-#def test_compute_residuals():
-#    try:
-#        residuals = compute_residuals(y, y_hat)
-#        residuals_match = residuals.round(2) == np.array(
-#            [21.72, -9.42, -4.69, 0.98, -12.6, -14.76, 16.72, 34.1, -12.48, -19.56]
-#        )
-#        test_passes = residuals_match.min()
-#    except:
-#        test_passes = False
-#    return test_passes
-#test_compute_residuals()    
-#
-#
-## ## Function to plot residuals against all input variables
-#
-## In[ ]:
-#
-#
-#def plot_residuals_against_inputs(X, residuals):
-#    """Plot residuals against input variables."""
-#    number_of_plots = len(X.columns)
-#    nrows = int(np.sqrt(number_of_plots))
-#    ncols = int(number_of_plots/nrows)
-#    fig, axs = plt.subplots(nrows=nrows, ncols=ncols)
-#    fig.set_figheight(6*nrows)
-#    fig.set_figwidth(8*ncols)
-#    for ax, col in zip(axs.flat, X.columns):
-#        sns.scatterplot(x=X[col], y=residuals, ax=ax)
-#        sns.lineplot(x=[X[col].min(), X[col].max()], y=[0, 0], ax=ax, color='red')
-#        ax.set_xlabel(col.title())
-#        ax.set_ylabel('Residuals')
-#        ax.set_title(f'Residuals vs {col.title()}')
-#
-#def test_plot_residuals_against_inputs():
-#    """Test the plot_residuals function."""
-#    try:
-#        plot_residuals_against_inputs(X, residuals)
-#        test_passes = True
-#    except:
-#        test_passes = False
-#    return test_passes
-#
-#test_plot_residuals_against_inputs()
-#
-#
-## ## Function to plot residuals against predicted values
-#
-## In[ ]:
-#
-#
-#def plot_residuals_against_prediction(y, residuals):
-#    """Plot residuals against predicted values."""
-#    ax = sns.scatterplot(
-#        x=y, 
-#        y=residuals
-#    )
-#    sns.lineplot(x=[y.min(), y.max()], y=[0, 0], color='red', ax=ax)
-#    ax.set(
-#        title='Residuals vs Predicted Values',
-#        xlabel='Predicted Values',
-#        ylabel='Residuals'
-#    )
-#
-#def test_plot_residuals_against_prediction():
-#    """Test the plot_residuals function."""
-#    try:
-#        plot_residuals_against_prediction(y, residuals)
-#        test_passes = True
-#    except:
-#        test_phistogramasses = False
-#    return test_passes
-#
-#test_plot_residuals_against_prediction()
-#
-#
-## In[ ]:
-#
-#
-### Function to plot distribution of residuals
-#
-#
-## In[ ]:
-#
-#
-#def plot_residuals_distribution(residuals):
-#    """Plot the distribution of normalized residuals."""
-#    normalized_residuals = residuals/residuals.std()
-#    ax = sns.distplot(normalized_residuals)
-#    ax.set(
-#        title='Distribution of Normalized Residuals.',
-#        xlabel='Residuals',
-#        ylabel='Probability Density'
-#    )
-#    
-#def test_plot_residuals_ditribution():
-#    """Test plot_residuals_, title='Test'distribution function"""
-#    try:
-#        plot_residuals_distribution(residuals)
-#        test_passes = True
-#    except:
-#        test_passes = False
-#    return test_passes
-#
-#test_plot_residuals_ditribution()
-#
-#
-## ## Function to make a QQ plot of residuals and conduct a Kolmogorov Smirnov test for normality 
-#
-## In[ ]:
-#
-#
-#def plot_residuals_normal_qq(residuals):
-#    """Make a QQ ploat of normalized residuals against a standard normal distribution."""
-#    plt.style.use('ggplot')
-#    normalized_residuals = residuals/residuals.std()
-#    sm.graphics.qqplot(normalized_residuals, dist=stats.norm, line='45', fit=True)
-#    kstest_result = stats.kstest(normalized_residuals, 'norm')
-#    if kstest_result.pvalue <= 0.05:
-#        print(f'We reject the null hypothesis that our residuals are normally distributed at the $\alpha =0.05$ level.')
-#    else:
-#        print(f'We fail to reject the null hypothesis that our residuals are normally distributed at the alpha = 0.05 level.')
-#
-#
-#def test_plot_residuals_ditribution():
-#    """Test plot_residuals_normal_qq function"""
-#    try:
-#        plot_residuals_normal_qq(residuals)
-#        test_passes = True
-#    except:
-#        test_passes = False
-#    return test_passes
-#
-#test_plot_residuals_ditribution()
-#
-#
-## ## Function to compute variance inflation factors for all input variables
-#
-## In[ ]:
-#
-#
-#def compute_vif(X):
-#    """Compute variance inflation factors for all input variables."""
-#    vifs = dict()
-#    for ind, col in enumerate(X.columns):
-#        vif_score = vif(np.matrix(X), ind)
-#        vifs[col] = vif_score.round(2)
-#    return vifs
-#
-#def test_compute_vif():
-#    """Test the compute_vif function."""
-#    ref_values = {'B': 3.3, 'C': 5.0, 'D': 5.41}
-#    vifs = compute_vif(X)
-#    tests = [vifs[col] == ref_values[col] for col in X.columns]
-#    test_passes = np.min(tests)
-#    return test_passes
-#
-#test_compute_vif()
-#
+
+    def test_predict_target(self):
+        """Test predict_target function."""
+        try:
+            y_hat = self.predict_target(self.X, self.model) 
+            prediction_match = y_hat.round(2) == np.array(
+                [29.28, 69.42, 78.69, 22.02, 13.6, 15.76, 15.28, 53.9, 53.48, 33.56]
+            )
+            test_passes = prediction_match.min()
+        except:
+            test_passes = False
+        return test_passes
+
+
+    def test_compute_residuals(self):
+        try:
+            residuals = self.compute_residuals(self.y, self.y_hat)
+            residuals_match = residuals.round(2) == np.array(
+                [21.72, -9.42, -4.69, 0.98, -12.6, -14.76, 16.72, 34.1, -12.48, -19.56]
+            )
+            test_passes = residuals_match.min()
+        except:
+            test_passes = False
+        return test_passes
+
+
+    def test_plot_residuals_against_inputs(self):
+        """Test the plot_residuals function."""
+        try:
+            self.plot_residuals_against_inputs(self.X, self.residuals)
+            test_passes = True
+        except:
+            test_passes = False
+        return test_passes
+
+
+    def test_plot_residuals_against_prediction(self):
+        """Test the plot_residuals function."""
+        try:
+            self.plot_residuals_against_prediction(self.y, self.residuals)
+            test_passes = True
+        except:
+            test_passes = False
+        return test_passes
+
+
+    def test_plot_residuals_ditribution(self):
+        """Test plot_residuals_, title='Test'distribution function"""
+        try:
+            self.plot_residuals_distribution(self.residuals)
+            test_passes = True
+        except:
+            test_passes = False
+        return test_passes
+
+
+    def test_plot_residuals_normal_qq(self):
+        """Test plot_residuals_normal_qq function"""
+        try:
+            self.plot_residuals_normal_qq(self.residuals)
+            test_passes = True
+        except:
+            test_passes = False
+        return test_passes
+
+
+    def test_compute_vif(self):
+        """Test the compute_vif function."""
+        ref_values = {'B': 3.3, 'C': 5.0, 'D': 5.41}
+        vifs = self.compute_vif(self.X)
+        tests = [vifs[col] == ref_values[col] for col in self.X.columns]
+        test_passes = np.min(tests)
+        return test_passes
