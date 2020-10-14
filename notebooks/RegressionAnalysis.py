e235e19ed3cd11f7a8df0658ba9ffcb067b81aca
Seth Chart <seth.chart@protonmail.com>
2020-10-14 16:26:18 -0400

    Refactoring RegressionAnalysis module

--- notebooks/RegressionAnalysis.py
+++ notebooks/RegressionAnalysis.py
@@ -1,4 +1,3 @@
-import os
 import pandas as pd
 import numpy as np
 import seaborn as sns
@@ -19,12 +18,12 @@ class Model(object):
         self.target = target
         self.X = self.data.drop(target, axis=1)
         self.y = self.data[target]
-        self.X_train, self.X_test, self.y_train, self.y_test = self._make_test_train_split()
+        self.X_train, self.X_test, self.y_train, self.y_test = self._make_test_train_split(self.X, self.y)
         self.model = self._fit_model(self.X_train, self.y_train)
-        self.y_hat_train = self._predict_target(self.X_train)
+        self.y_hat_train = self._predict_target(self.X_train, self.model)
         self.residuals_train = self._compute_residuals(self.y_train,
                                                       self.y_hat_train)
-
+#Public methods
 
     def make_pair_plot(self):
         """Given a dataframe make a pairplot of all variables."""
@@ -36,58 +35,33 @@ class Model(object):
         corr = self.data.corr()
         return sns.heatmap(corr, annot=True, cmap='Blues')
 
-    def _make_test_train_split(self):
-        from sklearn.model_selection import train_test_split
-        self.X_train, self.X_test, self.y_train, self.y_test = train_test_split(self.X, self.y, test_size=0.2, random_state=42)
-        return self.X_train, self.X_test, self.y_train, self.y_test
-
-
-    def _fit_model(self, X, y):
-        """Fit a least squares linear model to the training inputs X_train and training outputs y_train."""
-        from sklearn.linear_model import LinearRegression
-        linreg = LinearRegression(fit_intercept=True)
-        model = linreg.fit(X, y)
-        return model
-
-
-    def _predict_target(self, X):
-        """Use the provided model and data X to produce predicted values of the target."""
-        y_hat = self.model.predict(X)
-        return y_hat
-
-
-    def _compute_residuals(self, y, y_hat):
-        """Compute residuals from a series of observed values y and predicted values y_hat"""
-        return y-y_hat
-
-
-    def plot_residuals_against_inputs(self, X, residuals):
-        """Plot residuals against input variables."""
-        number_of_plots = len(X.columns)
+    def plot_residuals_against_inputs(self):
+        """Plot residuals_train against input variables."""
+        number_of_plots = len(self.X_train.columns)
         nrows = int(np.sqrt(number_of_plots))
         ncols = int(number_of_plots/nrows)
         fig, axs = plt.subplots(nrows=nrows, ncols=ncols)
         fig.set_figheight(6*nrows)
         fig.set_figwidth(8*ncols)
-        for ax, col in zip(axs.flat, X.columns):
-            sns.scatterplot(x=X[col], y=residuals, ax=ax)
-            sns.lineplot(x=[X[col].min(), X[col].max()], y=[0, 0], ax=ax, color='red')
+        for ax, col in zip(axs.flat, self.X_train.columns):
+            sns.scatterplot(x=self.X_train[col], y=self.residuals_train, ax=ax)
+            sns.lineplot(x=[self.X_train[col].min(), self.X_train[col].max()], y=[0, 0], ax=ax, color='red')
             ax.set_xlabel(col.title())
             ax.set_ylabel('Residuals')
             ax.set_title(f'Residuals vs {col.title()}')
         return fig
 
 
-    def plot_residuals_against_prediction(self, y_hat, residuals):
+    def plot_residuals_against_prediction(self):
         """Plot residuals against predicted values."""
         fig = plt.figure() 
         ax = plt.axes()
         sns.scatterplot(
-            x=y_hat, 
-            y=residuals,
+            x=self.y_hat_train, 
+            y=self.residuals_train,
             ax=ax
         )
-        sns.lineplot(x=[y_hat.min(), y_hat.max()], y=[0, 0], color='red', ax=ax)
+        sns.lineplot(x=[self.y_hat_train.min(), self.y_hat_train.max()], y=[0, 0], color='red', ax=ax)
         ax.set(
             title='Residuals vs Predicted Values',
             xlabel='Predicted Values',
@@ -96,12 +70,11 @@ class Model(object):
         return fig
 
 
-
-    def plot_residuals_distribution(self, residuals):
+    def plot_residuals_distribution(self):
         """Plot the distribution of normalized residuals."""
         fig = plt.figure()
         ax = plt.axes()
-        normalized_residuals = residuals/residuals.std()
+        normalized_residuals = self.residuals_train/self.residuals_train.std()
         sns.distplot(normalized_residuals, ax=ax)
         ax.set(
             title='Distribution of Normalized Residuals.',
@@ -111,11 +84,11 @@ class Model(object):
         return fig
 
 
-    def plot_residuals_normal_qq(self, residuals):
+    def plot_residuals_normal_qq(self):
         """Make a QQ ploat of normalized residuals against a standard normal distribution."""
         fig = plt.figure()
         ax = plt.axes()
-        normalized_residuals = residuals/residuals.std()
+        normalized_residuals = self.residuals_train/self.residuals_train.std()
         sm.graphics.qqplot(normalized_residuals, dist=stats.norm, line='45',
                            fit=True, ax=ax)
         return fig
@@ -127,13 +100,39 @@ class Model(object):
 #            print(f'We fail to reject the null hypothesis that our residuals are normally distributed at the alpha = 0.05 level.')
 
 
-    def compute_vif(self, X):
+    def compute_vif(self):
         """Compute variance inflation factors for all input variables."""
         vifs = dict()
-        for ind, col in enumerate(X.columns):
-            vif_score = vif(np.matrix(X), ind)
+        for ind, col in enumerate(self.X_train.columns):
+            vif_score = vif(np.matrix(self.X_train), ind)
             vifs[col] = vif_score.round(2)
         return vifs
+
+#Private methods
+
+    def _make_test_train_split(self, X, y):
+        from sklearn.model_selection import train_test_split
+        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+        return X_train, X_test, y_train, y_test
+
+
+    def _fit_model(self, X, y):
+        """Fit a least squares linear model to the training inputs X_train and training outputs y_train."""
+        from sklearn.linear_model import LinearRegression
+        linreg = LinearRegression(fit_intercept=True)
+        model = linreg.fit(X, y)
+        return model
+
+
+    def _predict_target(self, X, model):
+        """Use the provided model and data X to produce predicted values of the target."""
+        y_hat = model.predict(X)
+        return y_hat
+
+
+    def _compute_residuals(self, y, y_hat):
+        """Compute residuals from a series of observed values y and predicted values y_hat"""
+        return y-y_hat
 
 
 class _TestModel(Model):
@@ -141,14 +140,19 @@ class _TestModel(Model):
 
     def __init__(self):
         np.random.seed(42)
-        self.test_data = pd.DataFrame(np.random.randint(0,100,size=(10, 4)), columns=list('ABCD'))
-        self.test_target = 'A'
-        super().__init__(self.test_data , self.test_target)
+        self.data = pd.DataFrame(np.random.randint(0,100,size=(10, 4)), columns=list('ABCD'))
+        self.target = 'A'
+        self.X = self.data.drop(['A'], axis=1)
+        self.X_train = self.X
+        self.y = self.data['A']
+        #super().__init__(self.data , self.target)
         from sklearn.linear_model import LinearRegression
-        self.linreg = LinearRegression(fit_intercept=True)
-        self.model = self.linreg.fit(self.X, self.y)
+        linreg = LinearRegression(fit_intercept=True)
+        self.model = linreg.fit(self.X, self.y)
         self.y_hat = self.model.predict(self.X)
+        self.y_hat_train = self.y_hat
         self.residuals = self.y-self.y_hat
+        self.residuals_train = self.residuals
 
 
     def test_make_pair_plot(self):
@@ -174,11 +178,11 @@ class _TestModel(Model):
     def test_make_test_train_split(self):
         """Test the _make_test_train_split function."""
         try:
-            self._make_test_train_split()
-            comparison1 = self.X_test.sort_index() == self.X.iloc[[1,8],:]
-            comparison2 = self.X_train.sort_index() == self.X.drop([1,8], axis=0)
-            comparison3 = self.y_test.sort_index() == self.y[[1,8]]
-            comparison4 = self.y_train.sort_index() == self.y.drop([1,8])
+            X_train, X_test, y_train, y_test = self._make_test_train_split(self.X, self.y)
+            comparison1 = X_test.sort_index() == self.X.iloc[[1,8],:]
+            comparison2 = X_train.sort_index() == self.X.drop([1,8], axis=0)
+            comparison3 = y_test.sort_index() == self.y[[1,8]]
+            comparison4 = y_train.sort_index() == self.y.drop([1,8])
             test1 = comparison1.all(axis=None)
             test2 = comparison2.all(axis=None)
             test3 = comparison3.all(axis=None)
@@ -205,7 +209,7 @@ class _TestModel(Model):
     def test_predict_target(self):
         """Test _predict_target function."""
         try:
-            y_hat = self._predict_target(self.X) 
+            y_hat = self._predict_target(self.X, self.model) 
             prediction_match = y_hat.round(2) == np.array(
                 [29.28, 69.42, 78.69, 22.02, 13.6, 15.76, 15.28, 53.9, 53.48, 33.56]
             )
@@ -230,7 +234,7 @@ class _TestModel(Model):
     def test_plot_residuals_against_inputs(self):
         """Test the plot_residuals function."""
         try:
-            self.plot_residuals_against_inputs(self.X, self.residuals)
+            self.plot_residuals_against_inputs()
             test_passes = True
         except:
             test_passes = False
@@ -240,17 +244,17 @@ class _TestModel(Model):
     def test_plot_residuals_against_prediction(self):
         """Test the plot_residuals function."""
         try:
-            self.plot_residuals_against_prediction(self.y, self.residuals)
+            self.plot_residuals_against_prediction()
             test_passes = True
         except:
             test_passes = False
         return test_passes
 
 
-    def test_plot_residuals_ditribution(self):
-        """Test plot_residuals_, title='Test'distribution function"""
+    def test_plot_residuals_distribution(self):
+        """Test plot_residuals_distribution function"""
         try:
-            self.plot_residuals_distribution(self.residuals)
+            self.plot_residuals_distribution()
             test_passes = True
         except:
             test_passes = False
@@ -260,7 +264,7 @@ class _TestModel(Model):
     def test_plot_residuals_normal_qq(self):
         """Test plot_residuals_normal_qq function"""
         try:
-            self.plot_residuals_normal_qq(self.residuals)
+            self.plot_residuals_normal_qq()
             test_passes = True
         except:
             test_passes = False
@@ -270,7 +274,8 @@ class _TestModel(Model):
     def test_compute_vif(self):
         """Test the compute_vif function."""
         ref_values = {'B': 3.3, 'C': 5.0, 'D': 5.41}
-        vifs = self.compute_vif(self.X)
+        vifs = self.compute_vif()
         tests = [vifs[col] == ref_values[col] for col in self.X.columns]
         test_passes = np.min(tests)
         return test_passes
+
